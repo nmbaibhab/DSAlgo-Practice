48d1d4c1d54b34bc9027e1260a31c8f91c8460d6
Baibhab <nmbaibhab@gmail.com>
2021-01-10 21:21:41 +0530

    new file added

---
+++ Arrays/fighting_darkness.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+public class fighting_darkness {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		class Solution{
+		    long maxDays(long arr[], int n){
+		        Arrays.sort(arr);
+		       return arr[n-1];
+		    }
+		}
+
+	}
+
+}
